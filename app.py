ecd9cd47a6666103df75560b65b621b1732a7855
morikaglobal <sm+github@morikaglobal.com>
2021-02-23 21:54:35 +0900

    testing buildpack

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request
 
+import pygsheets
+client = pygsheets.authorize(service_file="google-credentials.json")
+sheet = client.open("COVID19_japan")
+wks = sheet[0]
 
 app = Flask(__name__)
 
 @app.route("/")
 def favicon():
-    return "Hello World"
+    
+    # get the current data date from google spreadsheet cell I2
+    
+    current_data = wks.get_value("I2")
+    print("CURRENT DATA IS " + current_data)
+    return "Hello buidpack success" 
 
 
 if __name__ == "__main__":
